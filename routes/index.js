4af56b1a79d9201e822fa3d85545459270493f1d
Nghia Do <nghia.do@vietan-software.com>
2020-03-01 16:52:23 +0700

    Create post router to register. (save user data registed in database)

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,63 @@ const jwt = require("jsonwebtoken");
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.set('views', '../views');
-app.use(express.static('public'))
+app.use(express.static('../public'));
 
-router.get('/', function(req, res, err){
+//body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
+
+//router
+const User = require('../models/user_model.js');
+
+//home page
+router.get('/', function(req, res){
     res.render('../views/home.ejs', { page: 'choice.ejs'});
-    
 });
 
+//sign page
 router.get('/sign', function (req, res) {
-    res.render('../views/home.ejs', { page: 'signin' });
+    res.render('../views/home.ejs', { page: 'signin.ejs' });
+});
+
+//register user
+router.post('/sign', async function(req,res){
+    var status = false;
+    if (req.body.alone) {
+        status = true;
+    };
+    var newuser = new User({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.pwd,
+        location: req.body.address,
+        phone: req.body.phone,
+        job: req.body.work,
+        workplace: req.body.workplace,
+        age: req.body.age,
+        favorite: req.body.favorite,
+        yourself: req.body.yourself,
+        security: req.body.security,
+        income: req.body.income,
+        alone: status
+    });
+
+    try{
+        const User = await newuser.save(function(err, data){
+            if (err) {
+                res.json({
+                    kq: 0,
+                    ErrMess: 'Loi~ ne`=====> ' + err
+                });
+                console.log(err);
+            } else {
+                // console.log('New User registed thanh` cong` '+ data);
+                res.render('../views/home.ejs', { page: 'chat' });
+            };
+        });
+    }catch(err){
+        res.status(400).send(err);
+    };
 });
 
 module.exports = router;
